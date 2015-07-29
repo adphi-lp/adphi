a80def94ed04450093fe86d8c3ae8871f0412cba
Jiahao Li <isundaylee.reg@gmail.com>
2015-07-29 02:33:06 -0700

    Shortlog for signatures.

--- app/controllers/signatures_controller.rb
+++ app/controllers/signatures_controller.rb
@@ -3,12 +3,14 @@ class SignaturesController < ApplicationController
   def sign
     @sig = Signature.find(params[:id])
     @sig.sign!
+    Shortlog.signature(@sig.brother, @sig.id, :sign)
     redirect_back root_path, success: 'You have successfully signed. '
   end
 
   def decline
     @sig = Signature.find(params[:id])
     @sig.decline!
+    Shortlog.signature(@sig.brother, @sig.id, :decline)
     redirect_back root_path, success: 'You have successfully declined the signature. '
   end
 
