d9c864262e8424f98c652000c41ba3a3785435d7
Jiahao Li <isundaylee.reg@gmail.com>
2016-02-11 22:18:44 -0500

    Potential fix for PDF content type issue.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -5,3 +5,7 @@ Paperclip::Attachment.default_options[:s3_credentials] = {
   secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
   s3_host_name: ENV['S3_HOST_NAME']
 }
+
+Paperclip.options[:content_type_mappings] = {
+  pdf: "application/pdf"
+}
