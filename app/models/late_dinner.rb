01891141e69b2a58f8a6a218483ae14008c9b044
Jiahao Li <isundaylee.reg@gmail.com>
2016-05-06 13:37:46 -0400

    One-time late dinners only on dinner weekdays.

--- app/models/late_dinner.rb
+++ app/models/late_dinner.rb
@@ -1,5 +1,12 @@
 class LateDinner < ActiveRecord::Base
+  # TODO put this somewhere instead of in two files (this and kitchen_controller.rb)
+  DINNER_WEEKDAYS = [0, 1, 2, 3, 4]   # We only have dinners Sun-Thur
+
   belongs_to :brother
+
+  def self.has_dinner_today?
+    DINNER_WEEKDAYS.include? Time.zone.now.wday
+  end
 
   def self.closed?
     Time.zone.now.hour >= 18
