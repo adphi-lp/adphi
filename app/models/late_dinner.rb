4487d552d8f47a6dada5bebbbfa92ea76afa1be9
Jiahao Li <isundaylee.reg@gmail.com>
2016-10-11 15:33:41 -0400

    More precise late dinner cutoff time.

--- app/models/late_dinner.rb
+++ app/models/late_dinner.rb
@@ -1,6 +1,7 @@
 class LateDinner < ActiveRecord::Base
   # TODO put this somewhere instead of in two files (this and kitchen_controller.rb)
   DINNER_WEEKDAYS = [0, 1, 2, 3, 4]   # We only have dinners Sun-Thur
+  LATE_DINNER_CUTOFF = 17.hours + 40.minutes
 
   belongs_to :brother
 
@@ -9,6 +10,15 @@ class LateDinner < ActiveRecord::Base
   end
 
   def self.closed?
-    Time.zone.now.hour >= 18
+    self.time_since_today >= LATE_DINNER_CUTOFF
   end
+
+  def self.time_until_close
+    LATE_DINNER_CUTOFF - self.time_since_today
+  end
+
+  private
+    def self.time_since_today
+      Time.zone.now - Time.zone.now.at_beginning_of_day
+    end
 end
