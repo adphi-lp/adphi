48cd1cecd83e894d122b0211c371ac8b7c70aebd
Jiahao Li <isundaylee.reg@gmail.com>
2016-10-10 21:50:14 -0400

    Uses AWS S3 only in production.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,11 +1,13 @@
-Paperclip::Attachment.default_options[:storage] = :s3
-Paperclip::Attachment.default_options[:s3_credentials] = {
-  bucket: ENV['S3_BUCKET_NAME'],
-  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-  s3_host_name: ENV['S3_HOST_NAME']
-}
+if Rails.env.production?
+  Paperclip::Attachment.default_options[:storage] = :s3
+  Paperclip::Attachment.default_options[:s3_credentials] = {
+    bucket: ENV['S3_BUCKET_NAME'],
+    access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+    s3_host_name: ENV['S3_HOST_NAME']
+  }
 
-Paperclip.options[:content_type_mappings] = {
-  pdf: "application/pdf"
-}
+  Paperclip.options[:content_type_mappings] = {
+    pdf: "application/pdf"
+  }
+end
